e2ef73faa8fd04abf4d56d0ed52aab1ef9d0ead5
Ad3b1y1 <opeoluwaadebiyi@outlook.com>
2022-12-22 04:12:46 +0100

    square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-Square = __import__('0-square').Square
-
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
-=======
 class Square:
     """Class Square that defines a square.
     """
     pass
->>>>>>> dc794ca4e825c5543cd7f32050740082c84a7942
